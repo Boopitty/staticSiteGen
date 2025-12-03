78d3d849c1ad3a3bf572cdf1bd52ee6cec8fdc16
Emil Auguste <emilrauguste@gmail.com>
2025-12-03 18:23:06 -0500

    changed the name of extract.py to inline.py

--- src/test_inline.py
+++ src/test_inline.py
@@ -1,4 +1,4 @@
-from extract import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
+from inline import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 from textnode import TextNode, TextType
 import unittest # unittest will run all test_functions
 
